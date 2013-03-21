7aa43f0778ac541125ffce78b3a8ea143a8a3548
igodip <igor.di.paolo@gmail.com>
2013-03-21 11:56:22 +0100

    Pagina corso e preferiti aggiunta!

---
+++ campus/src/gui/Corso.java
@@ -0,0 +1,14 @@
+package gui;
+
+import javax.swing.JPanel;
+
+public class Corso extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public Corso() {
+
+	}
+
+}
---
+++ campus/src/gui/Preferiti.java
@@ -0,0 +1,62 @@
+package gui;
+
+import javax.swing.JPanel;
+import java.awt.Rectangle;
+import java.awt.ComponentOrientation;
+import java.awt.Component;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JSeparator;
+import javax.swing.JDesktopPane;
+import javax.swing.border.LineBorder;
+import java.awt.FlowLayout;
+import java.awt.BorderLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.Canvas;
+
+public class Preferiti extends JPanel{
+	public Preferiti() {
+		setBackground(Color.WHITE);
+		setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.WHITE);
+		panel.setBounds(new Rectangle(0, 0, 1008, 429));
+		add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblPreferiti = new JLabel("Preferiti");
+		lblPreferiti.setForeground(new Color(6, 121, 159));
+		lblPreferiti.setFont(new Font("Arial", Font.BOLD, 20));
+		lblPreferiti.setBounds(10, 10, 200, 25);
+		panel.add(lblPreferiti);
+		
+		JSeparator separator = new JSeparator();
+		separator.setForeground(new Color(27, 50, 128));
+		separator.setBounds(0, 41, 170, 1);
+		panel.add(separator);
+		
+		JPanel preferito = new JPanel();
+		preferito.setBounds(50, 71, 400, 150);
+		panel.add(preferito);
+		preferito.setLayout(null);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		panel_2.setBounds(10, 21, 58, 24);
+		preferito.add(panel_2);
+		panel_2.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("APPUNTI");
+		lblNewLabel.setBounds(5, 2, 54, 20);
+		panel_2.add(lblNewLabel);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(Color.WHITE);
+		panel_1.setBounds(23, 11, 116, 128);
+		preferito.add(panel_1);
+		
+	}
+}
