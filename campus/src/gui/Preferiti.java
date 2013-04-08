1ca704743caf3fd604885399c2a6fbc02a23a44b
igodip <igor.di.paolo@gmail.com>
2013-04-08 16:17:44 +0200

    Creazione automatica dei preferiti!

--- campus/src/gui/Preferiti.java
+++ campus/src/gui/Preferiti.java
@@ -1,35 +1,43 @@
 package gui;
 
 import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Font;
 import java.awt.Rectangle;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
+import java.util.List;
 
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JSeparator;
 import javax.swing.border.LineBorder;
 
 import modello_di_dominio.Documento;
-import java.awt.LayoutManager;
-import java.awt.Component;
-import java.awt.FlowLayout;
 
 public class Preferiti extends JPanel{
 	
+	//Pannelli preferiti
 	private ArrayList<JPanel> preferiti = new ArrayList<JPanel>();
-	private Color preferitoBgColor = new Color(67,136,204);
+	
+	//Padding 
+	private int preferitoPadTop = 10;
+	private int preferitoPadLeft = 10;
+	private static Color preferitoBgColor = new Color(67,136,204);
+	
+	//Pannello principale
+	private JPanel panel;
+	
+	
 	//private 
 	
 	public Preferiti() {
 		setBackground(Color.WHITE);
 		setLayout(null);
 		
-		JPanel panel = new JPanel();
+		panel = new JPanel();
 		panel.setBackground(Color.WHITE);
 		panel.setBounds(new Rectangle(0, 0, 1008, 429));
 		add(panel);
@@ -46,57 +54,82 @@ public class Preferiti extends JPanel{
 		separator.setBounds(0, 41, 170, 1);
 		panel.add(separator);
 		
-		JPanel preferito = new JPanel();
-		preferito.setBackground(preferitoBgColor);
-		preferito.setBounds(50, 71, 400, 150);
-		panel.add(preferito);
-		preferito.setLayout(null);
+		ArrayList<Documento> prova = new ArrayList<Documento>();
+			
+		prova.add(null);
+		prova.add(null);
+		prova.add(null);
+		prova.add(null);
+		prova.add(null);
+		prova.add(null);
 		
-		JPanel panel_2 = new JPanel();
-		panel_2.setBorder(new LineBorder(new Color(0, 0, 0), 2));
-		panel_2.setBounds(10, 21, 58, 24);
-		preferito.add(panel_2);
-		panel_2.setLayout(null);
-		
-		JLabel lblNewLabel = new JLabel("APPUNTI");
-		lblNewLabel.setBackground(new Color(0xFF,0x99,0x00));
-		lblNewLabel.setBounds(5, 2, 54, 20);
-		panel_2.add(lblNewLabel);
-		
-		JPanel panel_1 = new JPanel();
-		panel_1.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent arg0) {
-				Home.openDocument(true);
-			}
-		});
-		panel_1.setBackground(Color.WHITE);
-		panel_1.setBounds(23, 11, 116, 128);
-		preferito.add(panel_1);
-		
-		JPanel panel_3 = new JPanel();
-		panel_3.setBounds(217, 116, 346, 218);
-		preferito.add(panel_3);
-		
-		JPanel panel_4 = new JPanel();
-		panel_3.add(panel_4);
+		addFavourites(prova);
 		
 	}
 	
 
 	/**
 	 * @wbp.factory
-	 * @wbp.factory.parameter.source background new java.awt.Color(67,136,204)
 	 * @wbp.factory.parameter.source layout null
 	 * @wbp.factory.parameter.source arg0 panel_2
 	 * @wbp.factory.parameter.source arg0_1 panel_1
 	 */
-	public static JPanel addPreferito(Color background, LayoutManager layout, Component arg0, Component arg0_1) {
-		JPanel panel = new JPanel();
-		panel.setBackground(background);
-		panel.setLayout(layout);
-		panel.add(arg0);
-		panel.add(arg0_1);
-		return panel;
+	public static JPanel createFavourite(Documento doc) {
+		
+		//Contenitore
+		JPanel preferito = new JPanel();
+		preferito.setBackground(preferitoBgColor);
+		
+		preferito.setLayout(null);
+		preferito.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		
+		preferito.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				Home.openDocument(true);
+			}
+		});
+		
+		//Anteprima
+		JPanel anteprima = new JPanel();
+		anteprima.setBackground(Color.WHITE);
+		anteprima.setBounds(23, 11, 116, 128);
+		
+		//Label panel
+		JPanel panelLabel = new JPanel();
+		panelLabel.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		panelLabel.setBounds(10, 21, 58, 24);
+		preferito.add(panelLabel);
+		panelLabel.setLayout(null);
+		
+		//Label
+		//TODO: Modificare in base al documento caricato
+		JLabel lblNewLabel = new JLabel("APPUNTI");
+		lblNewLabel.setBackground(new Color(0xFF,0x99,0x00));
+		lblNewLabel.setBounds(5, 2, 54, 20);
+		panelLabel.add(lblNewLabel);
+		preferito.add(anteprima);
+
+		return preferito;
+	}
+	
+	public void addFavourites(ArrayList<Documento> docs){
+		
+		
+		
+		for(int i = 0;i < docs.size();i++){
+			
+			JPanel preferito = createFavourite(docs.get(i));
+			
+			int col = (int) Math.floor(i/2);
+			int row = i%2;
+			
+			preferito.setBounds((50+(445*row)), 71+(180*col), 400, 150);
+			panel.add(preferito);
+			
+			panel.setBounds(panel.getX(),panel.getY(),panel.getWidth(),322+(180*col));
+			
+		}
+		
 	}
 }
