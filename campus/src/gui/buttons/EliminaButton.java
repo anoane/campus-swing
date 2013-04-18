0e744b7825a8a2f826085872bfe222dbde6da9f1
igodip <igor.di.paolo@gmail.com>
2013-04-18 17:11:02 +0200

    Modificati buttons

--- campus/src/gui/buttons/EliminaButton.java
+++ campus/src/gui/buttons/EliminaButton.java
@@ -2,11 +2,35 @@
  * 
  */
 package gui.buttons;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 
 /**
  * @author mw
  *
  */
 public class EliminaButton extends CustomButton {
-	
+	/**
+	 * 
+	 */
+	protected static String iconPath = "./newimage/elimina.png";
+	/**
+	 * 
+	 */
+	public EliminaButton() {
+		
+		super();
+		
+		setSize(52,52);
+		setBackground(null);
+		setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(iconPath));
+		lblNewLabel.setBounds(35, 11, 46, 30);
+		add(lblNewLabel);
+		
+		
+		
+	}
 }
