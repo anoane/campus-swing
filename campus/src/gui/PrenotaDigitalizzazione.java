9acafac602d6b23939b7c2133439a69c488cb8f7
igodip <igor.di.paolo@gmail.com>
2013-04-20 18:41:11 +0200

    Prenota digitalizzazione

--- campus/src/gui/PrenotaDigitalizzazione.java
+++ campus/src/gui/PrenotaDigitalizzazione.java
@@ -1,7 +1,49 @@
 package gui;
 
 import javax.swing.JPanel;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JSeparator;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.border.LineBorder;
 
 public class PrenotaDigitalizzazione extends JPanel {
+	public PrenotaDigitalizzazione() {
+		setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(null);
+		panel.setBackground(Color.WHITE);
+		panel.setBounds(0, 0, 1008, 800);
+		add(panel);
+		
+		JLabel label = new JLabel("Prenotazione Libri");
+		label.setForeground(new Color(6, 121, 159));
+		label.setFont(new Font("Arial", Font.BOLD, 20));
+		label.setBounds(10, 10, 200, 25);
+		panel.add(label);
+		
+		JSeparator separator = new JSeparator();
+		separator.setForeground(new Color(27, 50, 128));
+		separator.setBounds(0, 41, 170, 1);
+		panel.add(separator);
+		
+		JPanel btnRichiesta = new JPanel();
+		btnRichiesta.setBorder(new LineBorder(new Color(0x1D, 0x3B, 0x59), 2));
+		btnRichiesta.setBackground(new Color(46, 93, 140));
+		btnRichiesta.setBounds(153, 423, 157, 34);
+		panel.add(btnRichiesta);
+		
+		JLabel label_6 = new JLabel("Invia Richiesta");
+		label_6.setForeground(Color.WHITE);
+		label_6.setFont(new Font("Arial", Font.PLAIN, 18));
+		btnRichiesta.add(label_6);
+	}
 
 }
