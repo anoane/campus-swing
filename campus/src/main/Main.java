1cbb6cfe6cb43052be5dff9dd9776093bc30aac4
Simone <simondima89@gmail.com>
2013-04-22 10:03:35 +0200

    Modificato il main per caricare il db

--- campus/src/main/Main.java
+++ campus/src/main/Main.java
@@ -32,50 +32,47 @@ public class Main {
 			@Override
 			public void run() {
 				try {			
-					ControllerUtente u = ControllerUtente.getInstance();
-					ControllerAppunti d = ControllerAppunti.getInstance();
-					u.aggiungiDocumentoPreferito(u.getUtente(1), d.getAppunti(2));
-					
-					Home window = new Home();
-					window.getFrame().setVisible(true);
 					/*
 					ControllerCorso c = ControllerCorso.getInstance();
 					
-					//c.creaCorso("Sistemi Interattivi", "Nah cagata!");
+					c.creaCorso("Sistemi Interattivi", "Nah cagata!");
 					Corso corso = c.getCorso("Sistemi Interattivi");
 					
 					ControllerUniversita univ = ControllerUniversita.getInstance();
 					
-					//univ.createUniversita("Universita di L'Aquila");
+					univ.createUniversita("Universita di L'Aquila");
 					Universita universita = univ.getUniversita(1);
 					
 					ControllerFacolta f = ControllerFacolta.getInstance();
 					
-					//f.creaFacolta("Ingegneria",universita);
+					f.createFacolta("Ingegneria",universita.getID());
 					Facolta facolta = f.getFacolta(1);
 					
-					
-					
 					ControllerUtente u = ControllerUtente.getInstance();
-					//u.creaUtente("Pippo","Pluto","M",new Date(), facolta);
+					u.creaUtente("Pippo","Pluto","M",new Date(), facolta);
 					
-					//ControllerAppunti d = ControllerAppunti.getInstance();
+					ControllerAppunti d = ControllerAppunti.getInstance();
 					ControllerSlide s = ControllerSlide.getInstance();
 					Utente utente = u.getUtente("Pippo", "Pluto");
 					
 					
-					//d.creaAppunti("Sistemi", "Appunti", "/", utente, corso);
-					//d.creaAppunti("Interattivi", "Appunti", "/", utente, corso);
-					//s.creaSlide("Ingegneria del software", "Slide offerte dal professore", "/", utente, corso);
-					//JOptionPane.showMessageDialog(null, s.getSlide(4));
-					//u.aggiungiDocumentoPreferito(utente, s.getSlide(4));
-					//u.aggiungiDocumentoPreferito(utente, d.getAppunti(2));
+					d.creaAppunti("Sistemi", "Appunti", "/", utente, corso);
+					d.creaAppunti("Interattivi", "Appunti", "/", utente, corso);
+					s.creaSlide("Ingegneria del software", "Slide offerte dal professore", "/", utente, corso);
+					JOptionPane.showMessageDialog(null, s.getSlide(4));
+					u.aggiungiDocumentoPreferito(utente, s.getSlide(4));
+					u.aggiungiDocumentoPreferito(utente, d.getAppunti(2));
+					
+				
+					u.aggiungiDocumentoPreferito(u.getUtente(1), d.getAppunti(2));
+					u.aggiungiDocumentoPreferito(u.getUtente(1), s.getSlide(4));
+					
+					u.aggiungiDocumentoPreferito(u.getUtente(1), d.getAppunti(2));
 					*/
-					//ControllerAppunti d = ControllerAppunti.getInstance();
-					//ControllerSlide s = ControllerSlide.getInstance();
-					//ControllerUtente u = ControllerUtente.getInstance();
-					//u.aggiungiDocumentoPreferito(u.getUtente(1), d.getAppunti(2));
-					//u.aggiungiDocumentoPreferito(u.getUtente(1), s.getSlide(4));
+					Home window = new Home();
+					window.getFrame().setVisible(true);
+					
+					
 					
 				} catch (Exception e) {
 					e.printStackTrace();
