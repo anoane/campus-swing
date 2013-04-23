0da06cc14ba2b751b3a9044ba90835416ab11a27
igodip <igor.di.paolo@gmail.com>
2013-04-23 18:08:14 +0200

    Finalmente risolto GridBagLayout in PrenotaLibro

--- campus/src/gui/PrenotaLibro.java
+++ campus/src/gui/PrenotaLibro.java
@@ -17,11 +17,11 @@ import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 
 public class PrenotaLibro extends JPanel {
-	private JTextField textField_autore;
-	private JTextField textField_titolo;
-	private JTextField textField_casaEd;
-	private JTextField textField_ISBN;
-	private JTextField textField_;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
 	public PrenotaLibro() {
 		setLayout(null);
 		
@@ -43,14 +43,14 @@ public class PrenotaLibro extends JPanel {
 		panel.add(separator);
 		
 		JPanel formPanel = new JPanel();
-		formPanel.setBounds(22, 68, 399, 321);
+		formPanel.setBounds(34, 68, 363, 294);
 		formPanel.setBorder(new EmptyBorder(10,10,10,10));
 		formPanel.setBackground(null);
 		panel.add(formPanel);
 		GridBagLayout gbl_formPanel = new GridBagLayout();
-		gbl_formPanel.columnWidths = new int[]{189, 189, 0};
+		gbl_formPanel.columnWidths = new int[]{189, 189, 0, 0};
 		gbl_formPanel.rowHeights = new int[]{60, 60, 60, 60, 60, 0};
-		gbl_formPanel.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gbl_formPanel.columnWeights = new double[]{0.0, 1.0, 0.0, Double.MIN_VALUE};
 		gbl_formPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		formPanel.setLayout(gbl_formPanel);
 		
@@ -58,110 +58,106 @@ public class PrenotaLibro extends JPanel {
 		lbltitolo.setFont(new Font("Arial", Font.PLAIN, 14));
 		lbltitolo.setHorizontalAlignment(SwingConstants.LEFT);
 		GridBagConstraints gbc_lbltitolo = new GridBagConstraints();
-		gbc_lbltitolo.fill = GridBagConstraints.BOTH;
+		gbc_lbltitolo.anchor = GridBagConstraints.WEST;
+		gbc_lbltitolo.fill = GridBagConstraints.VERTICAL;
 		gbc_lbltitolo.insets = new Insets(0, 0, 5, 5);
 		gbc_lbltitolo.gridx = 0;
 		gbc_lbltitolo.gridy = 0;
 		formPanel.add(lbltitolo, gbc_lbltitolo);
 		
-		textField_titolo = new JTextField();
-		textField_titolo.setFont(new Font("Arial", Font.PLAIN, 14));
-		textField_titolo.setMargin(new Insets(10, 2, 2, 2));
-		GridBagConstraints gbc_textField_titolo = new GridBagConstraints();
-		gbc_textField_titolo.fill = GridBagConstraints.BOTH;
-		gbc_textField_titolo.insets = new Insets(0, 0, 5, 0);
-		gbc_textField_titolo.gridx = 1;
-		gbc_textField_titolo.gridy = 0;
-		formPanel.add(textField_titolo, gbc_textField_titolo);
-		textField_titolo.setColumns(15);
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 1;
+		gbc_textField.gridy = 0;
+		formPanel.add(textField, gbc_textField);
+		textField.setColumns(10);
 		
 		JLabel lblAutore = new JLabel("Autore");
 		lblAutore.setFont(new Font("Arial", Font.PLAIN, 14));
 		lblAutore.setHorizontalAlignment(SwingConstants.LEFT);
 		GridBagConstraints gbc_lblAutore = new GridBagConstraints();
-		gbc_lblAutore.fill = GridBagConstraints.BOTH;
+		gbc_lblAutore.anchor = GridBagConstraints.WEST;
+		gbc_lblAutore.fill = GridBagConstraints.VERTICAL;
 		gbc_lblAutore.insets = new Insets(0, 0, 5, 5);
 		gbc_lblAutore.gridx = 0;
 		gbc_lblAutore.gridy = 1;
 		formPanel.add(lblAutore, gbc_lblAutore);
 		
-		textField_autore = new JTextField();
-		textField_autore.setFont(new Font("Arial", Font.PLAIN, 14));
-		textField_autore.setMargin(new Insets(10, 2, 2, 2));
-		GridBagConstraints gbc_textField_autore = new GridBagConstraints();
-		gbc_textField_autore.fill = GridBagConstraints.BOTH;
-		gbc_textField_autore.insets = new Insets(0, 0, 5, 0);
-		gbc_textField_autore.gridx = 1;
-		gbc_textField_autore.gridy = 1;
-		formPanel.add(textField_autore, gbc_textField_autore);
-		textField_autore.setColumns(15);
+		textField_1 = new JTextField();
+		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_1.gridx = 1;
+		gbc_textField_1.gridy = 1;
+		formPanel.add(textField_1, gbc_textField_1);
+		textField_1.setColumns(10);
 		
 		JLabel lblCasaed = new JLabel("Casa editrice");
 		lblCasaed.setFont(new Font("Arial", Font.PLAIN, 14));
 		lblCasaed.setHorizontalAlignment(SwingConstants.LEFT);
 		GridBagConstraints gbc_lblCasaed = new GridBagConstraints();
-		gbc_lblCasaed.fill = GridBagConstraints.BOTH;
+		gbc_lblCasaed.anchor = GridBagConstraints.WEST;
+		gbc_lblCasaed.fill = GridBagConstraints.VERTICAL;
 		gbc_lblCasaed.insets = new Insets(0, 0, 5, 5);
 		gbc_lblCasaed.gridx = 0;
 		gbc_lblCasaed.gridy = 2;
 		formPanel.add(lblCasaed, gbc_lblCasaed);
 		
-		textField_casaEd = new JTextField();
-		textField_casaEd.setFont(new Font("Arial", Font.PLAIN, 14));
-		textField_casaEd.setMargin(new Insets(10, 2, 2, 2));
-		GridBagConstraints gbc_textField_casaEd = new GridBagConstraints();
-		gbc_textField_casaEd.fill = GridBagConstraints.BOTH;
-		gbc_textField_casaEd.insets = new Insets(0, 0, 5, 0);
-		gbc_textField_casaEd.gridx = 1;
-		gbc_textField_casaEd.gridy = 2;
-		formPanel.add(textField_casaEd, gbc_textField_casaEd);
-		textField_casaEd.setColumns(15);
+		textField_2 = new JTextField();
+		GridBagConstraints gbc_textField_2 = new GridBagConstraints();
+		gbc_textField_2.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_2.gridx = 1;
+		gbc_textField_2.gridy = 2;
+		formPanel.add(textField_2, gbc_textField_2);
+		textField_2.setColumns(10);
 		
 		JLabel lblCodISBN = new JLabel("Codice ISBN");
 		lblCodISBN.setFont(new Font("Arial", Font.PLAIN, 14));
 		lblCodISBN.setHorizontalAlignment(SwingConstants.LEFT);
 		GridBagConstraints gbc_lblCodISBN = new GridBagConstraints();
-		gbc_lblCodISBN.fill = GridBagConstraints.BOTH;
+		gbc_lblCodISBN.anchor = GridBagConstraints.WEST;
+		gbc_lblCodISBN.fill = GridBagConstraints.VERTICAL;
 		gbc_lblCodISBN.insets = new Insets(0, 0, 5, 5);
 		gbc_lblCodISBN.gridx = 0;
 		gbc_lblCodISBN.gridy = 3;
 		formPanel.add(lblCodISBN, gbc_lblCodISBN);
 		
-		textField_ISBN = new JTextField();
-		textField_ISBN.setFont(new Font("Arial", Font.PLAIN, 14));
-		textField_ISBN.setMargin(new Insets(10, 2, 2, 2));
-		GridBagConstraints gbc_textField_ISBN = new GridBagConstraints();
-		gbc_textField_ISBN.fill = GridBagConstraints.BOTH;
-		gbc_textField_ISBN.insets = new Insets(0, 0, 5, 0);
-		gbc_textField_ISBN.gridx = 1;
-		gbc_textField_ISBN.gridy = 3;
-		formPanel.add(textField_ISBN, gbc_textField_ISBN);
-		textField_ISBN.setColumns(15);
+		textField_3 = new JTextField();
+		GridBagConstraints gbc_textField_3 = new GridBagConstraints();
+		gbc_textField_3.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_3.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_3.gridx = 1;
+		gbc_textField_3.gridy = 3;
+		formPanel.add(textField_3, gbc_textField_3);
+		textField_3.setColumns(10);
 		
 		JLabel lblCitta = new JLabel("Citt\u00E0");
 		lblCitta.setFont(new Font("Arial", Font.PLAIN, 14));
 		lblCitta.setHorizontalAlignment(SwingConstants.LEFT);
 		GridBagConstraints gbc_lblCitta = new GridBagConstraints();
-		gbc_lblCitta.fill = GridBagConstraints.BOTH;
+		gbc_lblCitta.anchor = GridBagConstraints.WEST;
 		gbc_lblCitta.insets = new Insets(0, 0, 0, 5);
+		gbc_lblCitta.fill = GridBagConstraints.VERTICAL;
 		gbc_lblCitta.gridx = 0;
 		gbc_lblCitta.gridy = 4;
 		formPanel.add(lblCitta, gbc_lblCitta);
 		
-		textField_ = new JTextField();
-		textField_.setFont(new Font("Arial", Font.PLAIN, 14));
-		textField_.setMargin(new Insets(10, 2, 2, 2));
-		GridBagConstraints gbc_textField_ = new GridBagConstraints();
-		gbc_textField_.fill = GridBagConstraints.BOTH;
-		gbc_textField_.gridx = 1;
-		gbc_textField_.gridy = 4;
-		formPanel.add(textField_, gbc_textField_);
-		textField_.setColumns(15);
+		textField_4 = new JTextField();
+		GridBagConstraints gbc_textField_4 = new GridBagConstraints();
+		gbc_textField_4.insets = new Insets(0, 0, 0, 5);
+		gbc_textField_4.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_4.gridx = 1;
+		gbc_textField_4.gridy = 4;
+		formPanel.add(textField_4, gbc_textField_4);
+		textField_4.setColumns(10);
 		
 		JPanel panel_1 = new JPanel();
 		panel_1.setBorder(new LineBorder(new Color(0x1D, 0x3B, 0x59), 2));
 		panel_1.setBackground(new Color(46, 93, 140));
-		panel_1.setBounds(153, 423, 157, 34);
+		panel_1.setBounds(129, 404, 157, 34);
 		panel.add(panel_1);
 		
 		JLabel lblInvRic = new JLabel("Invia Richiesta");
