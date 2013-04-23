ba5b1edc1f42e4ac174a98fe98e7daac2b1d6fc6
igodip <igor.di.paolo@gmail.com>
2013-04-24 01:17:45 +0200

    PrenotaDigitalizzazione modificato :v

--- campus/src/gui/PrenotaDigitalizzazione.java
+++ campus/src/gui/PrenotaDigitalizzazione.java
@@ -14,6 +14,7 @@ import javax.swing.JTextField;
 import javax.swing.border.LineBorder;
 
 public class PrenotaDigitalizzazione extends JPanel {
+	private JTextField textField;
 	public PrenotaDigitalizzazione() {
 		setLayout(null);
 		
@@ -34,11 +35,87 @@ public class PrenotaDigitalizzazione extends JPanel {
 		separator.setBounds(0, 41, 170, 1);
 		panel.add(separator);
 		
+		JPanel formPanel = new JPanel();
+		formPanel.setBounds(21, 72, 415, 396);
+		formPanel.setBorder(new EmptyBorder(10,10,10,10));
+		formPanel.setBackground(null);
+		panel.add(formPanel);
+		GridBagLayout gbl_formPanel = new GridBagLayout();
+		gbl_formPanel.columnWidths = new int[]{189, 189, 0, 0};
+		gbl_formPanel.rowHeights = new int[] {60, 60, 60, 60, 60, 0, 0, 60, 0};
+		gbl_formPanel.columnWeights = new double[]{0.0, 1.0, 0.0, Double.MIN_VALUE};
+		gbl_formPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		formPanel.setLayout(gbl_formPanel);
+		
+		JLabel lblNomeDoc = new JLabel("Nome documento");
+		lblNomeDoc.setFont(new Font("Arial", Font.PLAIN, 14));
+		GridBagConstraints gbc_lblNomeDoc = new GridBagConstraints();
+		gbc_lblNomeDoc.anchor = GridBagConstraints.WEST;
+		gbc_lblNomeDoc.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNomeDoc.gridx = 0;
+		gbc_lblNomeDoc.gridy = 0;
+		formPanel.add(lblNomeDoc, gbc_lblNomeDoc);
+		
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 1;
+		gbc_textField.gridy = 0;
+		formPanel.add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		JLabel lblDesc = new JLabel("Descrizione");
+		lblDesc.setFont(new Font("Arial", Font.PLAIN, 14));
+		GridBagConstraints gbc_lblDesc = new GridBagConstraints();
+		gbc_lblDesc.anchor = GridBagConstraints.WEST;
+		gbc_lblDesc.insets = new Insets(0, 0, 5, 5);
+		gbc_lblDesc.gridx = 0;
+		gbc_lblDesc.gridy = 1;
+		formPanel.add(lblDesc, gbc_lblDesc);
+		
+		JLabel lblnumPag = new JLabel("Numero di pagine");
+		lblnumPag.setFont(new Font("Arial", Font.PLAIN, 14));
+		GridBagConstraints gbc_lblnumPag = new GridBagConstraints();
+		gbc_lblnumPag.anchor = GridBagConstraints.WEST;
+		gbc_lblnumPag.insets = new Insets(0, 0, 5, 5);
+		gbc_lblnumPag.gridx = 0;
+		gbc_lblnumPag.gridy = 2;
+		formPanel.add(lblnumPag, gbc_lblnumPag);
+		
+		JLabel lblmateria = new JLabel("Materia");
+		lblmateria.setFont(new Font("Arial", Font.PLAIN, 14));
+		GridBagConstraints gbc_lblmateria = new GridBagConstraints();
+		gbc_lblmateria.anchor = GridBagConstraints.WEST;
+		gbc_lblmateria.insets = new Insets(0, 0, 5, 5);
+		gbc_lblmateria.gridx = 0;
+		gbc_lblmateria.gridy = 3;
+		formPanel.add(lblmateria, gbc_lblmateria);
+		
+		JLabel lblcitta = new JLabel("Citt\u00E0");
+		lblcitta.setFont(new Font("Arial", Font.PLAIN, 14));
+		GridBagConstraints gbc_lblcitta = new GridBagConstraints();
+		gbc_lblcitta.anchor = GridBagConstraints.WEST;
+		gbc_lblcitta.insets = new Insets(0, 0, 5, 5);
+		gbc_lblcitta.gridx = 0;
+		gbc_lblcitta.gridy = 4;
+		formPanel.add(lblcitta, gbc_lblcitta);
+		
+		JLabel lblcopisteria = new JLabel("Copisteria");
+		lblcopisteria.setFont(new Font("Arial", Font.PLAIN, 14));
+		GridBagConstraints gbc_lblcopisteria = new GridBagConstraints();
+		gbc_lblcopisteria.anchor = GridBagConstraints.WEST;
+		gbc_lblcopisteria.gridheight = 2;
+		gbc_lblcopisteria.insets = new Insets(0, 0, 0, 5);
+		gbc_lblcopisteria.gridx = 0;
+		gbc_lblcopisteria.gridy = 5;
+		formPanel.add(lblcopisteria, gbc_lblcopisteria);
+		
 		JPanel btnRichiesta = new JPanel();
+		btnRichiesta.setBounds(154, 479, 130, 36);
+		panel.add(btnRichiesta);
 		btnRichiesta.setBorder(new LineBorder(new Color(0x1D, 0x3B, 0x59), 2));
 		btnRichiesta.setBackground(new Color(46, 93, 140));
-		btnRichiesta.setBounds(153, 423, 157, 34);
-		panel.add(btnRichiesta);
 		
 		JLabel label_6 = new JLabel("Invia Richiesta");
 		label_6.setForeground(Color.WHITE);
