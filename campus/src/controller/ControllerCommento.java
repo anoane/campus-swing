d51594e7c72937968f2983549b148b12db4b0499
igodip <igor.di.paolo@gmail.com>
2013-05-16 01:40:05 +0200

    Spero di non far danni!

--- campus/src/controller/ControllerCommento.java
+++ campus/src/controller/ControllerCommento.java
@@ -1,6 +1,11 @@
 package controller;
 
+import org.orm.PersistentException;
+
+import modello_di_dominio.Commento;
 import modello_di_dominio.DAOFactory;
+import modello_di_dominio.Documento;
+import modello_di_dominio.Utente;
 import modello_di_dominio.dao.CommentoDAO;
 
 /**
@@ -31,5 +36,34 @@ public class ControllerCommento extends AbstractController {
 		if(ControllerCommento.instance == null)
 			ControllerCommento.instance = new ControllerCommento();
 		return ControllerCommento.instance;
+	}
+	/**
+	 * aggiungiCommento
+	 * @param commento
+	 * @param d
+	 * @param u
+	 */
+	public void aggiungiCommento(String text,Documento d,Utente u){
+		
+		Commento c = commentoDAO.createCommento();
+		
+		c.setCommento(text);
+		c.setDocumento(d);
+		c.setUtente(u);
+		
+		try {
+			commentoDAO.save(c);
+		} catch (PersistentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	/**
+	 * aggiungiCommento
+	 * @param text
+	 * @param d
+	 */
+	public void aggiungiCommento(String text,Documento d){
+		this.aggiungiCommento(text, d, ControllerUtente.getInstance().getUtente(1));
 	}
 }
