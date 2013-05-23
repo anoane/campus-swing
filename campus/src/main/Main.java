e272e3a8b4ea85a13dfc3ead5c077e150260182d
Simone <simondima89@gmail.com>
2013-05-23 11:08:49 +0200

    fix del main

--- campus/src/main/Main.java
+++ campus/src/main/Main.java
@@ -28,51 +28,68 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		
+		/*
+		ControllerUniversita univ = ControllerUniversita.getInstance();
+		
+		univ.createUniversita("Universita di L'Aquila");
+		Universita universita = univ.getUniversita(1);
+		
+		ControllerFacolta f = ControllerFacolta.getInstance();
+		
+		f.createFacolta("Ingegneria",universita.getID());
+		Facolta facolta1 = f.getFacolta(1);
+		f.createFacolta("Scienze",universita.getID());
+		Facolta facolta2 = f.getFacolta(2);
+		
+		
+		ControllerCorso c = ControllerCorso.getInstance();
+		
+		c.creaCorso("Sistemi Interattivi", "Nah cagata!",facolta1.getID());
+		Corso corso = c.getCorso("Sistemi Interattivi");
+		
+		ControllerUtente u = ControllerUtente.getInstance();
+		u.creaUtente("Pippo","Pluto","PlutoPippo","M",new Date(), facolta1);
+		
+		ControllerDocumento d = ControllerDocumento.getInstance();
+		Utente utente = u.getUtente(1);
+		
+		
+		d.creaDocumento("Sistemi", "Appunti", "/","Slide", u.getUtente(1), corso, facolta1);
+		d.creaDocumento("Interattivi", "Appunti", "/","Appunti", utente, corso, facolta1);
+		d.creaDocumento("Sistemi", "Appunti", "/","Esercizi", u.getUtente(1), corso, facolta2);
+		d.creaDocumento("Interattivi", "Appunti", "/","Dispense", utente, corso, facolta2);
+		
+	
+		u.aggiungiDocumentoPreferito(u.getUtente(1), d.getDocumento(1));
+		u.aggiungiDocumentoPreferito(u.getUtente(1), d.getDocumento(2));
+		u.aggiungiDocumentoPreferito(u.getUtente(1), d.getDocumento(3));
+		u.aggiungiDocumentoPreferito(u.getUtente(1), d.getDocumento(4));
+		
+		u.aggiungiCorsoSeguito(utente,corso);
+		*/
+
+		
 		//Window thread
 		EventQueue.invokeLater(new Runnable() {
 			@Override
 			public void run() {
 				try {	
-					/*
-					ControllerUniversita univ = ControllerUniversita.getInstance();
+					ArrayList<Documento> docs = ControllerDocumento.getInstance().getListAllDocumenti();
+					ArrayList<Corso> corsi = ControllerCorso.getInstance().getListAllCorsi();
 					
-					univ.createUniversita("Universita di L'Aquila");
-					Universita universita = univ.getUniversita(1);
+					ControllerRicerca cr = ControllerRicerca.getInstance();
 					
-					ControllerFacolta f = ControllerFacolta.getInstance();
+					for(Documento doc : docs){
+						cr.aggiungiDocumento(doc);
+					}
 					
-					f.createFacolta("Ingegneria",universita.getID());
-					Facolta facolta1 = f.getFacolta(1);
-					f.createFacolta("Scienze",universita.getID());
-					Facolta facolta2 = f.getFacolta(2);
+					for(Corso corso : corsi){
+						cr.aggiungiCorso(corso);
+					}
 					
+					cr.commitIndexingDocumento();
+					cr.commitIndexingCorso();
 					
-					ControllerCorso c = ControllerCorso.getInstance();
-					
-					c.creaCorso("Sistemi Interattivi", "Nah cagata!",facolta1.getID());
-					Corso corso = c.getCorso("Sistemi Interattivi");
-					
-					ControllerUtente u = ControllerUtente.getInstance();
-					u.creaUtente("Pippo","Pluto","PlutoPippo","M",new Date(), facolta1);
-					
-					ControllerDocumento d = ControllerDocumento.getInstance();
-					Utente utente = u.getUtente(1);
-					
-					
-					d.creaDocumento("Sistemi", "Appunti", "/","Slide", u.getUtente(1), corso, facolta1);
-					d.creaDocumento("Interattivi", "Appunti", "/","Appunti", utente, corso, facolta1);
-					d.creaDocumento("Sistemi", "Appunti", "/","Esercizi", u.getUtente(1), corso, facolta2);
-					d.creaDocumento("Interattivi", "Appunti", "/","Dispense", utente, corso, facolta2);
-					
-				
-					u.aggiungiDocumentoPreferito(u.getUtente(1), d.getDocumento(1));
-					u.aggiungiDocumentoPreferito(u.getUtente(1), d.getDocumento(2));
-					u.aggiungiDocumentoPreferito(u.getUtente(1), d.getDocumento(3));
-					u.aggiungiDocumentoPreferito(u.getUtente(1), d.getDocumento(4));
-					
-					u.aggiungiCorsoSeguito(utente,corso);
-					*/
-
 
 					Home window = new Home();
 					window.getFrame().setVisible(true);
@@ -85,40 +102,6 @@ public class Main {
 			}
 		});
 		
-
-		EventQueue.invokeLater(new Runnable() {
-			@Override
-			public void run(){
-				ArrayList<Documento> docs = ControllerDocumento.getInstance().getListAllDocumenti();
-				ArrayList<Corso> corsi = ControllerCorso.getInstance().getListAllCorsi();
-				
-				ControllerRicerca cr = ControllerRicerca.getInstance();
-				
-				for(Documento doc : docs){
-					cr.aggiungiDocumento(doc);
-				}
-				
-				for(Corso corso : corsi){
-					cr.aggiungiCorso(corso);
-				}
-				
-				cr.commitIndexingDocumento();
-				cr.commitIndexingCorso();
-				
-				
-				/* TESTING ONLY
-				cr.removeDocumento(docs.get(docs.size()-1));
-				cr.commitIndexingDocumento();
-				
-				Documento[] result = cr.cercaDocumento("Descrizione", "Descrizione:App*");
-				
-				System.out.println(result.length);
-				
-				for(Documento doc : result){
-					System.out.println(doc.getNome());
-				}*/
-			}
-		});
 		
 	}
 
