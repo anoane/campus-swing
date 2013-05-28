bec8082d3e799e0a81f70fe024c848acedd1a54d
Fabiano Anemone <anos@hotmail.it>
2013-05-28 12:48:32 +0200

    WIP fix grafica corsopanel

--- campus/src/gui/CorsoPanel.java
+++ campus/src/gui/CorsoPanel.java
@@ -1,5 +1,6 @@
 package gui;
 
+import gui.helpers.JTextFieldLimit;
 import gui.riquadri.RiquadroDoc;
 
 import java.awt.Color;
@@ -25,11 +26,14 @@ import javax.swing.border.LineBorder;
 
 import org.orm.PersistentException;
 
+import controller.ControllerCorso;
 import controller.ControllerDocumento;
 import controller.ControllerUtente;
 
 import modello_di_dominio.Corso;
 import modello_di_dominio.Documento;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 @SuppressWarnings("serial")
 public class CorsoPanel extends Pagina {
@@ -45,7 +49,8 @@ public class CorsoPanel extends Pagina {
 	
 	private JPanel contenuto_pagina;
 	ButtonStandard buttonCreator = new ButtonStandard();
-	JButton seguiButton = buttonCreator.createButton("Segui Corso", 300, 11, 157, 34, false, true);
+	JButton seguiButton = buttonCreator.createButton("Aggiungi ai corsi seguiti", 740, 8, 230, 28, false, true);
+	JButton nonSeguiButton = buttonCreator.createButton("Rimuovi dai corsi seguiti", 740, 8, 230, 28, false, true);
 	
 	JLabel labelName = new JLabel();
 	JTextPane descrizione = new JTextPane();
@@ -68,56 +73,60 @@ public class CorsoPanel extends Pagina {
 		panel.setBounds(0, 0, 1008, 429);
 		add(panel);
 		
-		//Label
-		labelName.setForeground(new Color(6, 121, 159));
-		labelName.setFont(new Font("Arial", Font.BOLD, 20));
-		labelName.setBounds(10, 10, 280, 25);
-		panel.add(labelName);
-		
 		JSeparator separator = new JSeparator();
 		separator.setForeground(new Color(27, 50, 128));
 		separator.setBounds(0, 41, 170, 1);
 		panel.add(separator);
+		labelName.setText("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW");
+		
+		//Label
+		labelName.setForeground(new Color(6, 121, 159));
+		labelName.setFont(new Font("Arial", Font.BOLD, 20));
+		labelName.setBounds(10, 10, 723, 25);
+		panel.add(labelName);
 		
 		panel.add(seguiButton);
-				
+		panel.add(nonSeguiButton);
+			
 		JPanel panel_1 = new JPanel();
 		panel_1.setBackground(Color.WHITE);
-		panel_1.setBounds(10, 53, 988, 275);
+		panel_1.setBounds(10, 53, 988, 235);
 		panel.add(panel_1);
 		panel_1.setLayout(null);
+		descrizione.setFont(new Font("Arial", Font.PLAIN, 14));
 		
 		descrizione.setBackground(Color.WHITE);
 		descrizione.setMargin(new Insets(5, 5, 5, 5));
 		descrizione.setEditable(false);
-		descrizione.setBounds(10, 50, 460, 275);
+		descrizione.setBounds(0, 49, 478, 62);
 		panel_1.add(descrizione);
 		
 		JSeparator separator_1 = new JSeparator();
 		separator_1.setOrientation(SwingConstants.VERTICAL);
 		separator_1.setForeground(new Color(0x1B, 0x32, 0x80));
-		separator_1.setBounds(488, 10, 1, 275);
+		separator_1.setBounds(488, 10, 1, 215);
 		panel_1.add(separator_1);
 		
 		JLabel lblDescrizione = new JLabel("Descrizione");
 		lblDescrizione.setForeground(new Color(0x06, 0x79, 0x9F));
 		lblDescrizione.setFont(new Font("Arial", Font.PLAIN, 18));
 		lblDescrizione.setAlignmentX(Component.CENTER_ALIGNMENT);
-		lblDescrizione.setBounds(10, 10, 102, 28);
+		lblDescrizione.setBounds(0, 10, 102, 28);
 		panel_1.add(lblDescrizione);
 		
 		JPanel programma = new JPanel();
 		programma.setBackground(Color.WHITE);
-		programma.setBounds(508, 50, 460, 275);
+		programma.setBounds(500, 50, 460, 174);
 		panel_1.add(programma);
 		GridBagLayout gbl_formPanel = new GridBagLayout();
-		gbl_formPanel.columnWidths = new int[] {167, 171, 32, 0};
-		gbl_formPanel.rowHeights = new int[]{60, 60, 60, 60, 0};
-		gbl_formPanel.columnWeights = new double[]{0.0, 1.0, 1.0, Double.MIN_VALUE};
-		gbl_formPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_formPanel.columnWidths = new int[] {145, 171, 0};
+		gbl_formPanel.rowHeights = new int[]{30, 30, 60, 60, 0};
+		gbl_formPanel.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		gbl_formPanel.rowWeights = new double[]{1.0, 1.0, 1.0, 1.0, Double.MIN_VALUE};
 		programma.setLayout(gbl_formPanel);
 		
-		JLabel lblNewLabel = new JLabel("Università:");
+		JLabel lblNewLabel = new JLabel("Universit\u00E0");
+		lblNewLabel.setFont(new Font("Arial", Font.PLAIN, 14));
 		lblNewLabel.setAlignmentX(Component.RIGHT_ALIGNMENT);
 		lblNewLabel.setHorizontalAlignment(SwingConstants.LEFT);
 		lblNewLabel.setHorizontalTextPosition(SwingConstants.LEFT);
@@ -132,22 +141,61 @@ public class CorsoPanel extends Pagina {
 		
 		GridBagConstraints gbc_comboBox = new GridBagConstraints();
 		gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
-		gbc_comboBox.insets = new Insets(0, 0, 5, 5);
+		gbc_comboBox.insets = new Insets(0, 0, 5, 0);
 		gbc_comboBox.gridx = 1;
 		gbc_comboBox.gridy = 0;
+		comboBox.setFont(new Font("Arial", Font.PLAIN, 14));
 		programma.add(comboBox, gbc_comboBox);
 		
-		JLabel lblNewLabel_3 = new JLabel("Modalit\u00E0 d'esame:");
+		JTextArea textArea = new JTextArea();
+		GridBagConstraints gbc_textArea = new GridBagConstraints();
+		gbc_textArea.insets = new Insets(0, 0, 5, 0);
+		gbc_textArea.fill = GridBagConstraints.BOTH;
+		gbc_textArea.gridx = 2;
+		gbc_textArea.gridy = 0;
+		//programma.add(textArea, gbc_textArea);
+		
+		JTextArea textArea_1 = new JTextArea();
+		textArea.setFont(new Font("Arial", Font.PLAIN, 14));
+		textArea.setLineWrap(true);
+		textArea.setEnabled(false);
+		//textArea.setBorder(new LineBorder(new Color(0, 0, 0), 1));
+		textArea.setWrapStyleWord(true);
+		textArea.setDocument(new JTextFieldLimit(254));
+		textArea_1.setFont(new Font("Arial", Font.PLAIN, 14));
+		textArea_1.setLineWrap(true);
+		textArea_1.setEnabled(false);
+		//textArea.setBorder(new LineBorder(new Color(0, 0, 0), 1));
+		textArea_1.setWrapStyleWord(true);
+		textArea_1.setDocument(new JTextFieldLimit(254));
+		GridBagConstraints gbc_textArea_1 = new GridBagConstraints();
+		gbc_textArea_1.insets = new Insets(0, 0, 5, 0);
+		gbc_textArea_1.fill = GridBagConstraints.BOTH;
+		gbc_textArea_1.gridx = 2;
+		gbc_textArea_1.gridy = 1;
+		//programma.add(textArea_1, gbc_textArea_1);
+		
+		JScrollPane scrollPane = new JScrollPane(textArea);
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.insets = new Insets(0, 0, 5, 0);
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane.gridx = 1;
+		gbc_scrollPane.gridy = 2;
+		programma.add(scrollPane, gbc_scrollPane);
+		
+		JLabel lblNewLabel_3 = new JLabel("Modalit\u00E0 d'esame");
+		lblNewLabel_3.setFont(new Font("Arial", Font.PLAIN, 14));
 		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
-		gbc_lblNewLabel_3.anchor = GridBagConstraints.WEST;
-		gbc_lblNewLabel_3.insets = new Insets(0, 0, 5, 0);
+		gbc_lblNewLabel_3.anchor = GridBagConstraints.NORTHWEST;
+		gbc_lblNewLabel_3.insets = new Insets(0, 0, 0, 5);
 		gbc_lblNewLabel_3.gridx = 0;
 		gbc_lblNewLabel_3.gridy = 3;
 		programma.add(lblNewLabel_3, gbc_lblNewLabel_3);
 		
-		JLabel lblNewLabel_2 = new JLabel("Programma del corso:");
+		JLabel lblNewLabel_2 = new JLabel("Programma del corso");
+		lblNewLabel_2.setFont(new Font("Arial", Font.PLAIN, 14));
 		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
-		gbc_lblNewLabel_2.anchor = GridBagConstraints.WEST;
+		gbc_lblNewLabel_2.anchor = GridBagConstraints.NORTHWEST;
 		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNewLabel_2.gridx = 0;
 		gbc_lblNewLabel_2.gridy = 2;
@@ -155,14 +203,16 @@ public class CorsoPanel extends Pagina {
 		
 		
 		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setFont(new Font("Arial", Font.PLAIN, 14));
 		GridBagConstraints gbc_comboBox_1 = new GridBagConstraints();
-		gbc_comboBox_1.insets = new Insets(0, 0, 5, 5);
+		gbc_comboBox_1.insets = new Insets(0, 0, 5, 0);
 		gbc_comboBox_1.fill = GridBagConstraints.HORIZONTAL;
 		gbc_comboBox_1.gridx = 1;
 		gbc_comboBox_1.gridy = 1;
 		programma.add(comboBox_1, gbc_comboBox_1);
 		
 		JLabel lblNewLabel_1 = new JLabel("Docenti");
+		lblNewLabel_1.setFont(new Font("Arial", Font.PLAIN, 14));
 		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
 		gbc_lblNewLabel_1.anchor = GridBagConstraints.WEST;
 		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
@@ -170,24 +220,70 @@ public class CorsoPanel extends Pagina {
 		gbc_lblNewLabel_1.gridy = 1;
 		programma.add(lblNewLabel_1, gbc_lblNewLabel_1);
 		
+		JScrollPane scrollPane_1 = new JScrollPane(textArea_1);
+		GridBagConstraints gbc_scrollPane_1 = new GridBagConstraints();
+		gbc_scrollPane_1.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane_1.gridx = 1;
+		gbc_scrollPane_1.gridy = 3;
+		programma.add(scrollPane_1, gbc_scrollPane_1);
+		
 		
 		
 		JLabel lblProgramma = new JLabel("Docenti che tengono il corso: ");
 		lblProgramma.setForeground(new Color(0x06, 0x79, 0x9F));
 		lblProgramma.setFont(new Font("Arial", Font.PLAIN, 18));
 		lblProgramma.setAlignmentX(0.5f);
-		lblProgramma.setBounds(508, 10, 243, 28);
+		lblProgramma.setBounds(500, 10, 243, 28);
 		panel_1.add(lblProgramma);
 		
 		JSeparator separator_2 = new JSeparator();
 		separator_2.setForeground(new Color(27, 50, 128));
-		separator_2.setBounds(508, 38, 460, 1);
+		separator_2.setBounds(500, 38, 460, 1);
 		panel_1.add(separator_2);
+		
+		JTextPane facolta = new JTextPane();
+		facolta.setFont(new Font("Arial", Font.PLAIN, 14));
+		facolta.setMargin(new Insets(5, 5, 5, 5));
+		facolta.setEditable(false);
+		facolta.setBackground(Color.WHITE);
+		facolta.setBounds(0, 161, 478, 63);
+		panel_1.add(facolta);
+		if (corso != null) {
+			if (!corso.facolta.isEmpty()) {
+				for(int i=0; i<corso.facolta.size(); ++i){
+					if(i == 0){
+						facolta.setText(corso.facolta.toArray()[0].getNome());
+						++i;
+					}
+					if (i<corso.facolta.size()) {
+						facolta.setText(facolta.getText()+" , "+corso.facolta.toArray()[i].getNome());
+					}
+				}
+			}
+		}
+
+		
+		JLabel lblFacolt = new JLabel("Facolt\u00E0");
+		lblFacolt.setForeground(new Color(6, 121, 159));
+		lblFacolt.setFont(new Font("Arial", Font.PLAIN, 18));
+		lblFacolt.setAlignmentX(0.5f);
+		lblFacolt.setBounds(0, 122, 102, 28);
+		panel_1.add(lblFacolt);
+		
+		JSeparator separator_4 = new JSeparator();
+		separator_4.setForeground(new Color(27, 50, 128));
+		separator_4.setBounds(0, 38, 475, 1);
+		panel_1.add(separator_4);
+		
+		JSeparator separator_5 = new JSeparator();
+		separator_5.setForeground(new Color(27, 50, 128));
+		separator_5.setBounds(0, 150, 475, 1);
+		panel_1.add(separator_5);
 		
 
 		doc = new JPanel();
 		doc.setBackground(Color.WHITE);
-		doc.setBounds(0, 350, 1008, 352);
+		doc.setBounds(0, 300, 1008, 254);
 		doc.setLayout(null);
 		
 		panel.add(doc);
@@ -196,12 +292,12 @@ public class CorsoPanel extends Pagina {
 		lblPage = new JLabel("Materiale relativo al corso");
 		lblPage.setForeground(new Color(6, 121, 159));
 		lblPage.setFont(new Font("Arial", Font.BOLD, 20));
-		lblPage.setBounds(32, 10, 260, 25);
+		lblPage.setBounds(10, 10, 260, 25);
 		doc.add(lblPage);
 		
 		JSeparator separator_3 = new JSeparator();
 		separator_3.setForeground(new Color(27, 50, 128));
-		separator_3.setBounds(32, 41, 904, 1);
+		separator_3.setBounds(0, 41, 1006, 1);
 		doc.add(separator_3);
 		
 		contenuto_pagina = new JPanel();
@@ -222,8 +318,8 @@ public class CorsoPanel extends Pagina {
 		int altezza = 230*colmax;
 		
 		contenuto_pagina.setBounds(panel.getX(),panel.getY()+71,panel.getWidth(),altezza);
-		panel.setSize(panel.getWidth(), 420+altezza);
-		doc.setSize(doc.getWidth(), 420+altezza);
+		panel.setSize(panel.getWidth(), 370+altezza);
+		doc.setSize(doc.getWidth(), 370+altezza);
 		
 		for(int i = 0;i < docs.size(); i++){
 			int col = (int) Math.floor(i/2);
@@ -248,9 +344,17 @@ public class CorsoPanel extends Pagina {
 		adjustDocs(corso);
 		if(ControllerUtente.getInstance().containCorsoSeguito(ControllerUtente.getInstance().getUtente(1), corso)) {
 			seguiButton.setVisible(false);
+			nonSeguiButton.setVisible(true);
+			nonSeguiButton.addMouseListener(new MouseAdapter() {
+				public void mouseClicked(MouseEvent arg0){
+					ControllerUtente.getInstance().rimuoviCorsoSeguito(ControllerUtente.getInstance().getUtente(1), corso);
+					Home.openCorso(false, corso);
+				}
+			});
 		}
 		else{
 			seguiButton.setVisible(true);
+			nonSeguiButton.setVisible(false);
 			seguiButton.addMouseListener(new MouseAdapter() {
 				public void mouseClicked(MouseEvent arg0){
 					ControllerUtente.getInstance().aggiungiCorsoSeguito(ControllerUtente.getInstance().getUtente(1), corso);
@@ -278,6 +382,4 @@ public class CorsoPanel extends Pagina {
 		// TODO Auto-generated method stub
 		
 	}
-
-
 }
