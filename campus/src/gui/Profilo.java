a75160b8b78074d502f44b4089f0372400fb3eac
Fabiano Anemone <anos@hotmail.it>
2013-05-31 19:00:14 +0200

    profilo in progress

--- campus/src/gui/Profilo.java
+++ campus/src/gui/Profilo.java
@@ -45,20 +45,17 @@ public class Profilo extends Pagina {
 	private JPanel panelsx;
 	private JPanel doc;
 	private int altezzaPagina=0;
-	private JTextPane facolta = new JTextPane();
 	
 	//Label Pagina
 	private JLabel lblPage;
 	
 	private JPanel contenuto_pagina;
 	ButtonStandard buttonCreator = new ButtonStandard();
-	JButton seguiButton = buttonCreator.createButton("Aggiungi ai corsi seguiti", 740, 8, 230, 28, false, true);
-	JButton nonSeguiButton = buttonCreator.createButton("Rimuovi dai corsi seguiti", 740, 8, 230, 28, false, true);
+	//JButton seguiButton = buttonCreator.createButton("Aggiungi ai corsi seguiti", 740, 8, 230, 28, false, true);
+	//JButton nonSeguiButton = buttonCreator.createButton("Rimuovi dai corsi seguiti", 740, 8, 230, 28, false, true);
 	
 	JLabel labelName = new JLabel();
-	JTextPane descrizione = new JTextPane();
 	ArrayList<String> univ = new ArrayList<String>();
-	JComboBox comboBox = new JComboBox();
 
 	
 	/**
@@ -80,6 +77,7 @@ public class Profilo extends Pagina {
 		separator.setForeground(new Color(27, 50, 128));
 		separator.setBounds(0, 41, 170, 1);
 		panel.add(separator);
+		labelName.setText("Profilo");
 		//labelName.setText("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW");
 		
 		//Label
@@ -88,21 +86,14 @@ public class Profilo extends Pagina {
 		labelName.setBounds(10, 10, 723, 25);
 		panel.add(labelName);
 		
-		panel.add(seguiButton);
-		panel.add(nonSeguiButton);
+		//panel.add(seguiButton);
+		//panel.add(nonSeguiButton);
 			
 		JPanel panel_1 = new JPanel();
 		panel_1.setBackground(Color.WHITE);
 		panel_1.setBounds(10, 53, 988, 235);
 		panel.add(panel_1);
 		panel_1.setLayout(null);
-		descrizione.setFont(new Font("Arial", Font.PLAIN, 14));
-		
-		descrizione.setBackground(Color.WHITE);
-		descrizione.setMargin(new Insets(0, 0, 0, 0));
-		descrizione.setEditable(false);
-		descrizione.setBounds(0, 38, 478, 73);
-		panel_1.add(descrizione);
 		
 		JSeparator separator_1 = new JSeparator();
 		separator_1.setOrientation(SwingConstants.VERTICAL);
@@ -110,129 +101,26 @@ public class Profilo extends Pagina {
 		separator_1.setBounds(488, 10, 1, 215);
 		panel_1.add(separator_1);
 		
-		JLabel lblDescrizione = new JLabel("Descrizione");
-		lblDescrizione.setForeground(new Color(0x06, 0x79, 0x9F));
-		lblDescrizione.setFont(new Font("Arial", Font.PLAIN, 18));
-		lblDescrizione.setAlignmentX(Component.CENTER_ALIGNMENT);
-		lblDescrizione.setBounds(0, 10, 102, 28);
-		panel_1.add(lblDescrizione);
-		
-		JPanel programma = new JPanel();
-		programma.setBackground(Color.WHITE);
-		programma.setBounds(500, 50, 460, 174);
-		panel_1.add(programma);
-		GridBagLayout gbl_formPanel = new GridBagLayout();
-		gbl_formPanel.columnWidths = new int[] {145, 171, 0};
-		gbl_formPanel.rowHeights = new int[]{30, 30, 60, 60, 0};
-		gbl_formPanel.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
-		gbl_formPanel.rowWeights = new double[]{1.0, 1.0, 1.0, 1.0, Double.MIN_VALUE};
-		programma.setLayout(gbl_formPanel);
-		
-		JLabel lblNewLabel = new JLabel("Universit\u00E0");
-		lblNewLabel.setFont(new Font("Arial", Font.PLAIN, 14));
-		lblNewLabel.setAlignmentX(Component.RIGHT_ALIGNMENT);
-		lblNewLabel.setHorizontalAlignment(SwingConstants.LEFT);
-		lblNewLabel.setHorizontalTextPosition(SwingConstants.LEFT);
-		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
-		gbc_lblNewLabel.anchor = GridBagConstraints.WEST;
-		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel.gridx = 0;
-		gbc_lblNewLabel.gridy = 0;
-		programma.add(lblNewLabel, gbc_lblNewLabel);
-		
-		
-		
-		GridBagConstraints gbc_comboBox = new GridBagConstraints();
-		gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
-		gbc_comboBox.insets = new Insets(0, 0, 5, 0);
-		gbc_comboBox.gridx = 1;
-		gbc_comboBox.gridy = 0;
-		comboBox.setFont(new Font("Arial", Font.PLAIN, 14));
-		programma.add(comboBox, gbc_comboBox);
-		
-		JTextArea textArea = new JTextArea();
+		JLabel lblNomeUtente = new JLabel("");
+		lblNomeUtente.setForeground(new Color(0x06, 0x79, 0x9F));
+		lblNomeUtente.setFont(new Font("Arial", Font.PLAIN, 18));
+		lblNomeUtente.setAlignmentX(Component.CENTER_ALIGNMENT);
+		lblNomeUtente.setBounds(0, 10, 102, 28);
+		panel_1.add(lblNomeUtente);
 		GridBagConstraints gbc_textArea = new GridBagConstraints();
 		gbc_textArea.insets = new Insets(0, 0, 5, 0);
 		gbc_textArea.fill = GridBagConstraints.BOTH;
 		gbc_textArea.gridx = 2;
 		gbc_textArea.gridy = 0;
-		//programma.add(textArea, gbc_textArea);
-		
-		JTextArea textArea_1 = new JTextArea();
-		textArea.setFont(new Font("Arial", Font.PLAIN, 14));
-		textArea.setLineWrap(true);
-		textArea.setEnabled(false);
-		//textArea.setBorder(new LineBorder(new Color(0, 0, 0), 1));
-		textArea.setWrapStyleWord(true);
-		textArea.setDocument(new JTextFieldLimit(254));
-		textArea_1.setFont(new Font("Arial", Font.PLAIN, 14));
-		textArea_1.setLineWrap(true);
-		textArea_1.setEnabled(false);
-		//textArea.setBorder(new LineBorder(new Color(0, 0, 0), 1));
-		textArea_1.setWrapStyleWord(true);
-		textArea_1.setDocument(new JTextFieldLimit(254));
 		GridBagConstraints gbc_textArea_1 = new GridBagConstraints();
 		gbc_textArea_1.insets = new Insets(0, 0, 5, 0);
 		gbc_textArea_1.fill = GridBagConstraints.BOTH;
 		gbc_textArea_1.gridx = 2;
 		gbc_textArea_1.gridy = 1;
-		//programma.add(textArea_1, gbc_textArea_1);
-		
-		JScrollPane scrollPane = new JScrollPane(textArea);
-		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
-		gbc_scrollPane.insets = new Insets(0, 0, 5, 0);
-		gbc_scrollPane.fill = GridBagConstraints.BOTH;
-		gbc_scrollPane.gridx = 1;
-		gbc_scrollPane.gridy = 2;
-		programma.add(scrollPane, gbc_scrollPane);
-		
-		JLabel lblNewLabel_3 = new JLabel("Modalit\u00E0 d'esame");
-		lblNewLabel_3.setFont(new Font("Arial", Font.PLAIN, 14));
-		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
-		gbc_lblNewLabel_3.anchor = GridBagConstraints.NORTHWEST;
-		gbc_lblNewLabel_3.insets = new Insets(0, 0, 0, 5);
-		gbc_lblNewLabel_3.gridx = 0;
-		gbc_lblNewLabel_3.gridy = 3;
-		programma.add(lblNewLabel_3, gbc_lblNewLabel_3);
-		
-		JLabel lblNewLabel_2 = new JLabel("Programma del corso");
-		lblNewLabel_2.setFont(new Font("Arial", Font.PLAIN, 14));
-		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
-		gbc_lblNewLabel_2.anchor = GridBagConstraints.NORTHWEST;
-		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel_2.gridx = 0;
-		gbc_lblNewLabel_2.gridy = 2;
-		programma.add(lblNewLabel_2, gbc_lblNewLabel_2);
-		
-		
-		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setFont(new Font("Arial", Font.PLAIN, 14));
-		GridBagConstraints gbc_comboBox_1 = new GridBagConstraints();
-		gbc_comboBox_1.insets = new Insets(0, 0, 5, 0);
-		gbc_comboBox_1.fill = GridBagConstraints.HORIZONTAL;
-		gbc_comboBox_1.gridx = 1;
-		gbc_comboBox_1.gridy = 1;
-		programma.add(comboBox_1, gbc_comboBox_1);
-		
-		JLabel lblNewLabel_1 = new JLabel("Docenti");
-		lblNewLabel_1.setFont(new Font("Arial", Font.PLAIN, 14));
-		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
-		gbc_lblNewLabel_1.anchor = GridBagConstraints.WEST;
-		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel_1.gridx = 0;
-		gbc_lblNewLabel_1.gridy = 1;
-		programma.add(lblNewLabel_1, gbc_lblNewLabel_1);
-		
-		JScrollPane scrollPane_1 = new JScrollPane(textArea_1);
-		GridBagConstraints gbc_scrollPane_1 = new GridBagConstraints();
-		gbc_scrollPane_1.fill = GridBagConstraints.BOTH;
-		gbc_scrollPane_1.gridx = 1;
-		gbc_scrollPane_1.gridy = 3;
-		programma.add(scrollPane_1, gbc_scrollPane_1);
 		
 		
 		
-		JLabel lblProgramma = new JLabel("Docenti che tengono il corso");
+		JLabel lblProgramma = new JLabel("Ultime attivit\u00E0");
 		lblProgramma.setForeground(new Color(0x06, 0x79, 0x9F));
 		lblProgramma.setFont(new Font("Arial", Font.PLAIN, 18));
 		lblProgramma.setAlignmentX(0.5f);
@@ -244,31 +132,10 @@ public class Profilo extends Pagina {
 		separator_2.setBounds(500, 38, 460, 1);
 		panel_1.add(separator_2);
 		
-		facolta.setFont(new Font("Arial", Font.PLAIN, 14));
-		facolta.setMargin(new Insets(0, 0, 0, 0));
-		facolta.setEditable(false);
-		facolta.setBackground(Color.WHITE);
-		facolta.setBounds(0, 150, 478, 74);
-		panel_1.add(facolta);
-
-
-		
-		JLabel lblFacolt = new JLabel("Facolt\u00E0");
-		lblFacolt.setForeground(new Color(6, 121, 159));
-		lblFacolt.setFont(new Font("Arial", Font.PLAIN, 18));
-		lblFacolt.setAlignmentX(0.5f);
-		lblFacolt.setBounds(0, 122, 102, 28);
-		panel_1.add(lblFacolt);
-		
 		JSeparator separator_4 = new JSeparator();
 		separator_4.setForeground(new Color(27, 50, 128));
 		separator_4.setBounds(0, 38, 475, 1);
 		panel_1.add(separator_4);
-		
-		JSeparator separator_5 = new JSeparator();
-		separator_5.setForeground(new Color(27, 50, 128));
-		separator_5.setBounds(0, 150, 475, 1);
-		panel_1.add(separator_5);
 		
 
 		doc = new JPanel();
@@ -279,7 +146,7 @@ public class Profilo extends Pagina {
 		panel.add(doc);
 		
 		
-		lblPage = new JLabel("Materiale relativo al corso");
+		lblPage = new JLabel("Documenti caricati");
 		lblPage.setForeground(new Color(6, 121, 159));
 		lblPage.setFont(new Font("Arial", Font.BOLD, 20));
 		lblPage.setBounds(10, 10, 260, 25);
@@ -295,9 +162,25 @@ public class Profilo extends Pagina {
 		contenuto_pagina.setBackground(Color.WHITE);
 		doc.add(contenuto_pagina);
 	
-		separator_2.setVisible(false);
-		separator_4.setVisible(false);
-		separator_5.setVisible(false);
+		separator_2.setVisible(true);
+		separator_4.setVisible(true);
+		
+		lblNomeUtente.setText(utente.getNome()+" "+utente.getCognome());
+		
+		JLabel imgUtente = new JLabel("");
+		imgUtente.setBounds(10, 50, 175, 175);
+		panel_1.add(imgUtente);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(Color.WHITE);
+		panel_2.setBounds(195, 50, 283, 175);
+		panel_1.add(panel_2);
+		GridBagLayout gbl_panel_2 = new GridBagLayout();
+		gbl_panel_2.columnWidths = new int[]{0};
+		gbl_panel_2.rowHeights = new int[]{0};
+		gbl_panel_2.columnWeights = new double[]{Double.MIN_VALUE};
+		gbl_panel_2.rowWeights = new double[]{Double.MIN_VALUE};
+		panel_2.setLayout(gbl_panel_2);
 
 	}
 	
@@ -359,10 +242,7 @@ public class Profilo extends Pagina {
 
 	@Override
 	public void reload() {
-		adjustDocs(getDocs("",""));
-
-		DefaultComboBoxModel newModel = new DefaultComboBoxModel(univ.toArray());
-		comboBox.setModel(newModel);
+		adjustDocs(getDocs("all","timestampDOWN"));
 		//addDocumenti(documento);
 	}
 }
