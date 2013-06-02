9122f6a92cd30686f0f301c16f118f5756599b82
Fabiano Anemone <anos@hotmail.it>
2013-06-02 20:11:01 +0200

    update jOpenDocument to stable version, fix carica materiale message

--- campus/src/gui/CaricaMateriale.java
+++ campus/src/gui/CaricaMateriale.java
@@ -439,7 +439,7 @@ public class CaricaMateriale extends Pagina  {
 				}
 				if (continua) {
 					ControllerDocumento.getInstance().creaDocumento(textField.getText(), textArea.getText(), target.toString(), comboBox.getSelectedItem().toString(), Home.getUtenteLoggato(), ControllerCorso.getInstance().getCorso(textField_4.getText()),ControllerFacolta.getInstance().getFacolta(dbIndexFac));
-					JOptionPane.showMessageDialog(Home.getFrame(), "OK CI SIAMO", "Attenzione", JOptionPane.WARNING_MESSAGE);
+					JOptionPane.showMessageDialog(Home.getFrame(), "Documento caricato correttamente!", "Caricamento documento", JOptionPane.INFORMATION_MESSAGE);
 					Home.getPagina("miei_documenti");
 						
 				}
